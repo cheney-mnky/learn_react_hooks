8971b009bedce0419b38809dfffdafb0ac3c5e56
huyiyi <hu1111@qq.com>
2018-10-18 15:04:16 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import { combineReducers } from 'redux';
+import headerReducer from './routes/header/reducer';
+import sideReducer from './routes/side/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = combineReducers({
+    header: headerReducer,
+    side: sideReducer
+}) 
+// 创建store
+const store = createStore(reducers);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+ document.getElementById('root'));
+
 serviceWorker.unregister();
